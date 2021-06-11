4799254a8746bb7d40418a4d6d49baab5271c938
amruta-Khamkar <amrutakhamkar15@gmail.com>
2021-06-12 00:01:55 +0530

    firstCommit

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("This is my First Calculator and Project 7")
+
+let screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+
+for (button of buttons) {
+    button.addEventListener("click", (e) => {
+        buttonText = e.target.innerText;
+        console.log("button text is", buttonText);
+        if (buttonText == "X") {
+            buttonText = "*";
+            screenValue += buttonText;
+            screen.value=screenValue;
+        }
+        else if (buttonText == "C") {
+            screenValue =" ";
+            screen.value=screenValue;
+        }
+        else if (buttonText == "=") {
+            screen.value=eval(screenValue);
+        }
+        else{
+            screenValue += buttonText;
+            screen.value=screenValue;
+        }
+    })
+}
